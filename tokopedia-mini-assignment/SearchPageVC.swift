86edfd5c53c4f2c3ceaa91a8d1dc190e37b4b915
R. Kukuh <rkukuh@gmail.com>
2018-03-17 16:24:43 +0700

    Get products data from API as plain JSON

--- tokopedia-mini-assignment/SearchPageVC.swift
+++ tokopedia-mini-assignment/SearchPageVC.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SearchPageVC: UIViewController {
     
     let API_URL = "https://ace.tokopedia.com/search/v2.5/product?q=samsung&pmin=10000&pmax=100000&wholesale=true&official=true&fshop=2&start=0&rows=10"
+    
+    let params : [String : Any] = [
+        "q" : "samsung",
+        "pmin" : "10000",
+        "pmax" : "100000",
+        "wholesale" : true,
+        "official" : true,
+        "fshop" : "2",
+        "start" : "0",
+        "rows" : "10"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getProductsFromApi(url: API_URL, parameters: params)
+    }
+    
+    func getProductsFromApi(url: String, parameters: [String : Any]) {
+    
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            (response) in
+            
+            if response.result.isSuccess {
+                print("SUCCESS getting products data from API")
+                
+                let ProductsJSON : JSON = JSON(response.result.value!)
+                
+                print(ProductsJSON)
+                
+            } else {
+                print("ERROR: \(String(describing: response.result.error))")
+            }
+        }
     }
 
 }
