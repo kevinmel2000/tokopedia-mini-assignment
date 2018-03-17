c369881a8b3a49f865d00baf5d542cad0391112f
R. Kukuh <rkukuh@gmail.com>
2018-03-17 15:28:54 +0700

    Add API URL

--- tokopedia-mini-assignment/SearchPageVC.swift
+++ tokopedia-mini-assignment/SearchPageVC.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class SearchPageVC: UIViewController {
+    
+    let API_URL = "https://ace.tokopedia.com/search/v2.5/product?q=samsung&pmin=10000&pmax=100000&wholesale=true&official=true&fshop=2&start=0&rows=10"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
