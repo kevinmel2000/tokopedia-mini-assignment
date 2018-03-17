d227bdee202922a25a955d429ca732c666b5f1d6
R. Kukuh <rkukuh@gmail.com>
2018-03-17 17:27:26 +0700

    Add segue to Filter page VC

---
+++ tokopedia-mini-assignment/Controller/FilterPageVC.swift
@@ -0,0 +1,18 @@
+//
+//  FilterPageVC.swift
+//  tokopedia-mini-assignment
+//
+//  Created by R. Kukuh on 17/03/18.
+//  Copyright © 2018 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class FilterPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+
+}
